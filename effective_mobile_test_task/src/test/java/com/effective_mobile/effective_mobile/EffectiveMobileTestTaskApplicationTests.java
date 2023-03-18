9d8ca4b312c66dae506091ace9aa9d1e526fc031
Ekyd <Ekyd777@gmail.com>
2023-03-18 11:01:30 +0300

    init projeckt

---
+++ effective_mobile_test_task/src/test/java/com/effective_mobile/effective_mobile/EffectiveMobileTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effective_mobile.effective_mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveMobileTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
